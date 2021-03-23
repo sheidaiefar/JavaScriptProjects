fe25af3499d22e2d8c2b89c72456be8fc2a78112
malihe <sheidaiefar@gmail.com>
2021-03-23 12:24:17 +0430

    add

--- Calculator/assets/scripts/app.js
+++ Calculator/assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert('This works!');
+let currentResult = 0;
+let result = 0;
+
+function add() {
+    currentResult = result;
+    result = result + parseInt(currentInputValue.value);
+    // alert(`curr : ${currentResult} , res: ${result}`);
+    outputResult(result, ` ${currentResult} + ${currentInputValue.value}`);
+}
--- Calculator/assets/scripts/vendor.js
+++ Calculator/assets/scripts/vendor.js
@@ -1,13 +1,13 @@
-const userInput = document.getElementById('input-number');
+const currentInputValue = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
 
 const currentResultOutput = document.getElementById('current-result');
-const currentCalculationOutput = document.getElementById('current-calculation');
+const currentCalculationDescription = document.getElementById('current-calculation-description');
 
-function outputResult(result, text) {
+function outputResult(result, desc) {
   currentResultOutput.textContent = result;
-  currentCalculationOutput.textContent = text;
+  currentCalculationDescription.textContent = desc;
 }
